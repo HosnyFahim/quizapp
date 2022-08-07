b470e685e5553ff607ef52f9a8faf6ac2d995c04
HosnyFahim <hosny.fahim1990@gmail.com>
2022-08-07 18:05:30 +0200

    questions changed

--- array.js
+++ array.js
@@ -1,66 +1,66 @@
 let questions = [
 
     {
-        "question": "wer hat HTML erfunden?",
-        "answer_1": "Robbie Wolliams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
+        "question": "25 + 25 =",
+        "answer_1": "47",
+        "answer_2": "58",
+        "answer_3": "50",
+        "answer_4": "44",
         "right_answer": 3
     },
 
     {
-        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
-        "answer_1": "Text Fett",
-        "answer_2": "Container",
-        "answer_3": "Ein Link",
-        "answer_4": "Kursiv",
+        "question": "10 + 7 + 3 =",
+        "answer_1": "18",
+        "answer_2": "22",
+        "answer_3": "20",
+        "answer_4": "31",
         "right_answer": 3
     },
 
     {
-        "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
-        "answer_2": "&lt;iframe&gt;",
-        "answer_3": "&lt;frame&gt;",
-        "answer_4": "&lt;frameset&gt;",
+        "question": "10 x 10 =",
+        "answer_1": "100",
+        "answer_2": "1000",
+        "answer_3": "10000",
+        "answer_4": "100000",
+        "right_answer": 1
+    },
+
+    {
+        "question": "9 + 3 + 6 =",
+        "answer_1": "16",
+        "answer_2": "18",
+        "answer_3": "19",
+        "answer_4": "24",
         "right_answer": 2
     },
 
     {
-        "question": "Wie stellt man Text am BESTEN fett dar?",
-        "answer_1": "&lt;strong&gt",
-        "answer_2": "CSS nutzen",
-        "answer_3": "&lt;bild&gt;",
-        "answer_4": "&lt;b&gt;",
-        "right_answer": 1
+        "question": "2 x 4 + 5 =",
+        "answer_1": "11",
+        "answer_2": "12",
+        "answer_3": "13",
+        "answer_4": "15",
+        "right_answer": 3
     },
 
     {
-        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly",
-        "answer_2": "max",
-        "answer_3": "form",
-        "answer_4": "spellcheck",
-        "right_answer": 1
-    },
-
-    {
-        "question": "Wie wählst du alle Elemente von Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": "a[title]{...}",
-        "answer_2": "a > title {...}",
-        "answer_3": "a.title {...}",
-        "answer_4": "a=title {...}",
-        "right_answer": 1
-    },
-
-    {
-        "question": "Wie definiert man in JavaScript eine Variable?",
-        "answer_1": "let 100 = rate;",
-        "answer_2": "100 = let rate;",
-        "answer_3": "rate = 100;",
-        "answer_4": "let rate = 100;",
+        "question": "14 - 4 + 5 =",
+        "answer_1": "9",
+        "answer_2": "12",
+        "answer_3": "13",
+        "answer_4": "15",
         "right_answer": 4
+    },
+
+    {
+        "question": "200 - 200 + 200",
+        "answer_1": "0",
+        "answer_2": "100",
+        "answer_3": "200",
+        "answer_4": "400",
+        "right_answer": 3
     },
 
 ];
